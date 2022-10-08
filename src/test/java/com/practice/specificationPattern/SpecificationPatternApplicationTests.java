7e61b0a8e96782e2b5acd5985e1d86a364efcdfa
jeayoung <k01066624566@gmail.com>
2022-10-08 18:27:08 +0900

    오늘은 너무 피곤하니까 프로젝트 생성만 할래 읅

---
+++ src/test/java/com/practice/specificationPattern/SpecificationPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.specificationPattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecificationPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
